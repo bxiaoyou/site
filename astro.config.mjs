875b7d8cdcad1ded3b84177c0e0e768c740b946d
Xiaoyou Zhang <bxiaoyou@outlook.com>
2023-11-17 16:45:01 +0800

    ci: use vercel to deploy site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import solidJs from "@astrojs/solid-js";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.bxiaoyou.com",
   integrations: [svelte(), tailwind(), mdx(), solidJs()],
+  output: "server",
+  adapter: vercel(),
 });
